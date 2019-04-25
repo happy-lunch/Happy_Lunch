e44121a2fcfffbcb79e5831587df35c436423112
kngan99 <ngan.le.bku@hcmut.edu.vn>
2019-04-26 00:30:32 +0700

    Update Home Admin 2

--- main/java/com/cnpm/happylunch/ad_home_activity.java
+++ main/java/com/cnpm/happylunch/ad_home_activity.java
@@ -10,11 +10,11 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-import com.cnpm.happylunch.fragment.CartFragment;
-import com.cnpm.happylunch.fragment.HomeFragment;
-import com.cnpm.happylunch.fragment.ProfileFragment;
-import com.cnpm.happylunch.fragment.SettingFragment;
-import com.cnpm.happylunch.helper.BottomNavigationBehavior;
+import com.cnpm.happylunch.fragment.CartsFragment;
+import com.cnpm.happylunch.fragment.LogOutFragment;
+import com.cnpm.happylunch.fragment.MenuFragment;
+import com.cnpm.happylunch.fragment.OrderedFragment;
+import com.cnpm.happylunch.Admin_BottomNavigationBehavior;
 
 public class HomeAdmin extends AppCompatActivity {
 
